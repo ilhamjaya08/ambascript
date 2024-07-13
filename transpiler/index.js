74012359c724da38ff90a4a347f1a097497d7ec0
Muhammad Ilham Jaya <m.ilhamjaya1808@gmail.com>
2024-07-13 18:35:24 +0800

    feat: math transpiler

--- transpiler/index.js
+++ transpiler/index.js
@@ -1,8 +1,10 @@
 const { transpileIfElse } = require("./ifElse");
+const { transpileMath } = require("./math");
 const { transpileVarDeclaration } = require("./varDeclaration");
 
 module.exports = function (code) {
   code = transpileIfElse(code);
   code = transpileVarDeclaration(code);
+  code = transpileMath(code);
   return code;
 }
