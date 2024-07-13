f6e359e3dbb1903d91af9336181065fb3cd3241e
Muhammad Ilham Jaya <m.ilhamjaya1808@gmail.com>
2024-07-13 20:27:31 +0800

    feat: var declaration transpiler

--- transpiler/varDeclaration.js
+++ transpiler/varDeclaration.js
@@ -9,5 +9,5 @@ exports.transpileVarDeclaration = function (code) {
   return code
   .replace(/am (.+) =/g, (_, variable) => `let ${variable} =`)
   .replace(/jmk (.+) =/g, (_, variable) => `const ${variable} =`)
-  .replace(/var (.+) =/g, (_, variable) => `var ${variable} =`);
+  .replace(/\$(\w+) = '([^']+)'/g, (_, variable, value) => `var ${variable} = '${value}'`);
 }
